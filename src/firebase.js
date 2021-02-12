24500ad1ee248242f9a0ce710a85c1ce92b4e2c1
reno <73044693+renodesu@users.noreply.github.com>
2021-02-12 16:34:29 +1100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/database"
 
 const app = firebase.initializeApp ({
-    apiKey: "AIzaSyC5uizu4AhJlldqohiIj4hwSBPhpt8vnQs",
-    authDomain: "sa-app-da971.firebaseapp.com",
-    projectId: "sa-app-da971",
-    storageBucket: "sa-app-da971.appspot.com",
-    messagingSenderId: "165767989141",
-    appId: "1:165767989141:web:a8749db1930345f384989c",
-    measurementId: "G-ND736VX5CW"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   });
 
 export const db = app.database();
